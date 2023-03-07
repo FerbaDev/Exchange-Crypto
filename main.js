dba754aa5549f8d0ed59124d9ed38052d7612ee4
FerbaDev <elferbarron1@gmail.com>
2023-03-07 02:04:15 -0300

    se agrega un 2do parámetro a las funciones

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let peso, dolar, euro;
-peso = "peso";
-dolar = "dolar";
-euro = "euro";
+peso = "pesos";
+dolar = "dolares";
+euro = "euros";
 let ars, usd, eur;
 ars = 1;
 usd = 365;
@@ -9,23 +9,23 @@ eur = 380;
 alert("Bienvenido al exchange")
 
 let select1, select2;
-select1 = prompt("Seleccione moneda a vender: peso, dolar o euro.");
-select2 = prompt('Seleccione moneda a recibir: peso, dolar o euro.');
+select1 = prompt("Seleccione moneda a vender: pesos, dolares o euros.");
+select2 = prompt('Seleccione moneda a recibir: pesos, dolares o euros.');
 let monto = Number.parseInt(prompt("Ingrese monto:"))
-function venderPesos(moneda2) {
-    alert("Vas a recibir: " + monto / moneda2);
+function venderPesos(moneda2, ref) {
+    alert("Vas a recibir: " + monto / moneda2 + " " + ref);
 }
-function comprarPesos(moneda2) {
-    alert("Vas a recibir: " + monto * moneda2);
+function comprarPesos(moneda2, ref) {
+    alert("Vas a recibir: " + monto * moneda2 + " " + ref);
 }
 if (select1 == peso && select2 == dolar) {
-    venderPesos(usd);
+    venderPesos(usd, dolar);
 } else if (select1 == peso && select2 == euro) {
-    venderPesos(eur);
+    venderPesos(eur, euro);
 } else if (select1 == dolar && select2 == peso){
-    comprarPesos(usd);
+    comprarPesos(usd, peso);
 } else if (select1 == euro && select2 == peso) {
-    comprarPesos(eur);
+    comprarPesos(eur, peso);
 } else {
     alert("Esa opción no está disponible")
 };
