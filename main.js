0eb4b10b39637839cc3c4e619b641922c32bd285
FerbaDev <elferbarron1@gmail.com>
2023-03-07 02:34:31 -0300

    se añade un if else mas general haciendo que el monto ingresado sea mayor a 10

--- main.js
+++ main.js
@@ -12,20 +12,28 @@ let select1, select2;
 select1 = prompt("Seleccione moneda a vender: pesos, dolares o euros.");
 select2 = prompt('Seleccione moneda a recibir: pesos, dolares o euros.');
 let monto = Number.parseInt(prompt("Ingrese monto:"))
-function venderPesos(moneda2, ref) {
-    alert("Vas a recibir: " + monto / moneda2 + " " + ref);
-}
-function comprarPesos(moneda2, ref) {
-    alert("Vas a recibir: " + monto * moneda2 + " " + ref);
-}
-if (select1 == peso && select2 == dolar) {
-    venderPesos(usd, dolar);
-} else if (select1 == peso && select2 == euro) {
-    venderPesos(eur, euro);
-} else if (select1 == dolar && select2 == peso){
-    comprarPesos(usd, peso);
-} else if (select1 == euro && select2 == peso) {
-    comprarPesos(eur, peso);
+if (monto < 10) {
+    while (monto < 10) {
+        alert("El monto debe ser mayor a $10");
+        monto += 10;
+        alert("Recargue la página");
+    }
 } else {
-    alert("Esa opción no está disponible")
-};
+    function venderPesos(moneda2, ref) {
+        alert("Vas a recibir: " + monto / moneda2 + " " + ref);
+    }
+    function comprarPesos(moneda2, ref) {
+        alert("Vas a recibir: " + monto * moneda2 + " " + ref);
+    }
+    if (select1 == peso && select2 == dolar) {
+        venderPesos(usd, dolar);
+    } else if (select1 == peso && select2 == euro) {
+        venderPesos(eur, euro);
+    } else if (select1 == dolar && select2 == peso){
+        comprarPesos(usd, peso);
+    } else if (select1 == euro && select2 == peso) {
+        comprarPesos(eur, peso);
+    } else {
+        alert("Esa opción no está disponible")
+    };
+}
