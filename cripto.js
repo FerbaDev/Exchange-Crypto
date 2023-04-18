861b7d440d37cc89bbc946fd4318fec9770f3113
FerbaDev <elferbarron1@gmail.com>
2023-04-17 23:40:17 -0300

    se vuelve a crear metodo, logro pasarle un parametro, funciona!!

--- cripto.js
+++ cripto.js
@@ -7,6 +7,11 @@ class Cripto {
         this.chain = chain;
         this.precio = precio;
     }
+    comprar(monto) {
+        let recibe = document.getElementById("recibe");
+        recibe.innerHTML = `<p>vas a recibir ${monto / this.precio} ${this.nombre}</p>`;
+        localStorage.setItem("compra", `se compra ${monto / this.precio} ${this.nombre} por ${monto} USD`);
+    }
 }
 
 const bitcoin = new Cripto(1, "Bitcoin", "BTC", "bitcoin", 28000);
@@ -22,48 +27,34 @@ const polkadot = new Cripto(10, "Polkadot", "DOT", "bsc", 6.2);
 
 const criptomonedas = [bitcoin, ether, tether, bnb, ripple, uniswap, pancakeSwap, aave, dogecoin, polkadot];
 
-//header
+
 let todasLasCriptos = criptomonedas.map((cripto) => cripto.nombre + " está en " + "$" + cripto.precio);
 let header = document.querySelector("header");
 header.append(todasLasCriptos.join(" // "));
 
-// select
+
 const listaCriptos = document.getElementById("mi-select");
 
 for (cripto of criptomonedas) {
     listaCriptos.innerHTML += `<option class="criptoElegida" value="${cripto.nombre}">${cripto.nombre}</option>`
 }
 
-// inputs
 
 const botonComprar = document.querySelector(".botonComprar");
 
-botonComprar.addEventListener('click', comprarCripto);
-
-function comprarCripto(e) {
-    e.preventDefault();
-
-    const criptoSeleccionada = document.querySelector("#mi-select");
-    console.log(criptoSeleccionada.value);
-    const criptoElegida = criptomonedas.find((cripto) => cripto.nombre == criptoSeleccionada.value);
-    console.log(criptoElegida);
-
+botonComprar.addEventListener('click', () => {
+    const criptoElegida = criptomonedas.find((cripto) => cripto.nombre == listaCriptos.value);
     let ingreseMonto = document.getElementById("monto");
-
-    let recibe = document.getElementById("recibe");
-
-    let total = ingreseMonto.value / criptoElegida.precio;
-    
-    recibe.innerHTML = `<p>vas a recibir ${total} ${criptoElegida.nombre}</p>`;
-    localStorage.setItem("compra", `se compra ${total} ${criptoElegida.nombre} por ${ingreseMonto.value} USD`);
-}
+    let monto = parseInt(ingreseMonto.value);
+    criptoElegida.comprar(monto);
+});
 
 const resetear = document.querySelector(".cero")
 resetear.addEventListener("click", () => {
     recibe.innerHTML = `<p></p>`;
 })
 
-// modo oscuro
+
 let botonCambioModo = document.querySelector(".cambioModo");
 const body = document.body;
 
@@ -96,14 +87,15 @@ botonCambioModo.addEventListener("click", () => {
     }
 })
 
-// raking cripto
 
 const tabla = document.querySelector("#tabla");
 const botonesRanking = document.querySelectorAll(".botonRanking");
 
-function cargarRanking() {
+function cargarRanking(criptosFiltradas) {
+
+    tabla.innerHTML = "";
     
-    criptomonedas.forEach(criptomoneda => {
+    criptosFiltradas.forEach(criptomoneda => {
         const fila = document.createElement("tr");
         fila.innerHTML = `
             <td>${criptomoneda.rank}</td>
@@ -116,14 +108,19 @@ function cargarRanking() {
     })
 }
 
-cargarRanking();
+cargarRanking(criptomonedas);
 
 botonesRanking.forEach(boton => {
     boton.addEventListener("click", (e) => {
 
         botonesRanking.forEach(boton => boton.classList.remove("active"));
-
         e.currentTarget.classList.add("active");
 
+        if (e.currentTarget.id != "todas") {
+            const criptoFilter = criptomonedas.filter(criptomoneda => criptomoneda.chain === e.currentTarget.id);
+            cargarRanking(criptoFilter);
+        } else {
+            cargarRanking(criptomonedas)
+        }
     })
 })
